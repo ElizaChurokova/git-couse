e4572ca11d9b676ce8fc5aabfa86dfba32e428b7
ElizaChurokova <74547095+ElizaChurokova@users.noreply.github.com>
2024-08-25 00:06:43 +0600

    Update Main.java
    test 3 is added to the main

--- demo-example-for-git/src/Main.java
+++ demo-example-for-git/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Test 2");
+        System.outprintln("Test 3");
 
     }
 }
